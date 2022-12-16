358e3113c3f4380762f9eb1ae2fc0389aeabfcb9
40115 <40115@ufp.edu.pt>
2022-12-16 22:10:24 +0000

    Inicial Stance

---
+++ main.py
@@ -0,0 +1,18 @@
+# file: main.py
+import pycom
+import sys
+import time
+import os
+import machine
+from network import WLAN
+import ujson
+import ufun
+from mqtt import MQTTClient
+from wificon import WiFiClient
+
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
