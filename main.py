53311d38a97e23b9c6581df3ae06367b2be47134
40115 <40115@ufp.edu.pt>
2023-01-05 22:15:06 +0000

    Joao é este

--- main.py
+++ main.py
@@ -14,17 +14,39 @@ from wificon import WiFiClient
 from machine import ADC, Pin, PWM
 import utime
 
-
-pwm_12 = PWM(0, frequency=50)
 Pin_12='P12'
 Pin_16='P16'
+pwm_12 = PWM(0, frequency=50)
+pwm_c12 = pwm_12.channel(0, pin=Pin_12, duty_cycle=0.05)
 adc = ADC(0)
 adc.vref(1120)
 authorization_Key = 'HMSRJMTN97XVL8VW'
 Http_Update_host = 'https://api.thingspeak.com/'
-
 ssid = 'Vodafone-Pinto'
 net_key = 'helenaruben64'
+
+mosquitto_ip = "mqtt3.thingspeak.com"
+    #Connect to mosquito
+mosquitto_port = 1883
+device_id = "NiYOAQgOICktLjosKCEFIQY"
+mosquitto_uname="NiYOAQgOICktLjosKCEFIQY"
+mosquitto_pass="dl3b/+K57J++iZ4ffxtJ4/dF"
+mqttclient=None
+channel_id="1998372"
+
+def sub_cb(topic, msg):
+   print("sub_cb(): received on topic={} the msg={}\n".format(topic.decode(), msg.decode()))
+
+def init_mqtt():
+    value=read_light()
+    value=4095-value
+    value=int(value*100/4095)
+    print(value)
+    connect_to_wifi_network_wap2(wlan_mode=WLAN.STA,net_ssid=ssid, key=net_key)
+    mqttclient_Connect(value)
+    pwm_c12 = pwm_12.channel(0, pin=Pin_12, duty_cycle=0.05)
+    #pwm_c.duty_cycle(0.09 ) # change the duty cycle to 30%
+    #mqttclient_pushing(value)
 
 def init():
     value=read_light()
@@ -32,32 +54,66 @@ def init():
     value=int(value*100/4095)
     print(value)
     connect_to_wifi_network_wap2(wlan_mode=WLAN.STA,net_ssid=ssid, key=net_key)
-    pwm_c = pwm_12.channel(0, pin=Pin_12, duty_cycle=0.05)
+    pwm_c12 = pwm_12.channel(0, pin=Pin_12, duty_cycle=0.05)
     #pwm_c.duty_cycle(0.09 ) # change the duty cycle to 30%
     get_http_call(Http_Update_host,value)
 
-def ran_check_Wifi()
+def rancheckwifi():
     while True:
         value=read_light()
         value=4095-value
         value=int(value*100/4095)
         print(value)
-        pwm_c = pwm_12.channel(0, pin=Pin_12, duty_cycle=0.05)
+        pwm_c12 = pwm_12.channel(0, pin=Pin_12, duty_cycle=0.05)
         #pwm_c.duty_cycle(0.09 ) # change the duty cycle to 30%
         get_http_call(Http_Update_host,value)
-        check_Servo_Curtina(value)
+        checkservocurtina(value)
         time.sleep(5)
 
-def check_Servo_Curtina(value=100)
+def rancheckmqtt():
+    while True:
+        value=read_light()
+        value=4095-value
+        value=int(value*100/4095)
+        print(value)
+        pwm_c12 = pwm_12.channel(0, pin=Pin_12, duty_cycle=0.05)
+        #pwm_c.duty_cycle(0.09 ) # change the duty cycle to 30%
+        #get_http_call(Http_Update_host,value)
+        mqttclient_pushing(value)
+        checkservocurtina(value)
+        time.sleep(5)
+
+def mqttclient_Connect(value):
+
+    print('main(): create MQTT client to broker on addr = {}, port = {}...'.format(mosquitto_ip, mosquitto_port))
+    mqttclient = MQTTClient(device_id, mosquitto_ip, user=mosquitto_uname, password=mosquitto_pass, port=mosquitto_port)
+    #mqttclient = MQTTClient(device_id, mosquitto_ip, port=mosquitto_port)
+    try:
+        print('main(): set callback and connect to broker...')
+        ##Set a callback listening a topic
+        mqttclient.set_callback(sub_cb)
+        mqttclient.connect()
+        topic = "channels/" + channel_id + "/publish"
+        tpayload="field1="+str(value)+"&status=MQTTPUBLISH"
+        print("main(): Sending sensorvalue={}".format(value))
+        mqttclient.publish(topic=topic, msg="{"+tpayload+"}")
+    except OSError as err:
+        print('main(): cannot connect to broker on addr = {} with err = {}...\n'.format(mosquitto_ip, err))
+
+def mqttclient_pushing(values):
+    pass
+
+
+
+def checkservocurtina(value=100):
     if value<30:
-        pwm_c.duty_cycle(0.1 )
+        pwm_c12.duty_cycle(0.1 )
         time.sleep(1)
-        pwm_c.duty_cycle(0.05 )
+        pwm_c12.duty_cycle(0.05 )
 
 def get_http_call(url=Http_Update_host,value=-1):
     try:
-        Test="2020/10/10T20:20:20"
-        tesr="{}update?api_key={}&field1={}&field2={}".format(url, authorization_Key, str(value),Test )
+        tesr="{}update?api_key={}&field1={}".format(url, authorization_Key, str(value) )
         # Content-Type HTTP header should be set only for PUT and POST requests
         request = requests.get(tesr)
         reply=request.content.decode()
@@ -112,8 +168,9 @@ def connect_to_wifi_network_wap2(wlan_mode=WLAN.STA, net_ssid='ssid', sec_proto=
 
 def main():
     print("Hello World!")
-    init()
-
+    #init()
+    #rancheckwifi()
+    init_mqtt()
 
 
 
